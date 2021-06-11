17e81546d57da0ba9554da19b49938d85026fcbb
salahadell <salah-adel2011@hotmail.com>
2021-06-11 02:43:56 +0200

    Update miniMax.py

--- miniMax.py
+++ miniMax.py
@@ -1,31 +1,65 @@
 from evalHeuristics import *
+import math
+from makeMove import *
+from evalHeuristics import *
+from playerOne import *
+from playerTwo import *
+from winner_winner import *
 
-def miniMax(stonesAmount, depth, alpha, beta, isMaximizing=True):
+
+moveCharP1 = ['a','b','c','d','e','f']
+moveCharP2 = ['f','e','d','c','b','a']   
+
+
+def miniMax(stonesAmount, depth, alpha, beta, isMaximizing=True, playAgain=False):
 
     if depth == 0:
-        return evalHeuristics(stonesAmount)
+        return evalHeuristics(stonesAmount,playAgain,isMaximizing),-200
+    
+    elif (winner(stonesAmount)[2]==1):
+        return evalHeuristics(winner(stonesAmount)[0],playAgain,isMaximizing),-200    
         
     if isMaximizing:
         maxEval = -math.inf
         
         for i in range(6):
-            newState =  makeMove(stonesAmount,moveCharP1[i],True)          #3ayez makeMove teraga3 door anhy player
-            Eval = miniMax(newState, depth - 1, alpha, beta, False)        #ab3at lel makeMove fih stealing wala la2
-            maxEval = max(maxEval, Eval)                                   #ne7ot ehtmal en yekon el pocket fady
-            alpha = max(alpha, Eval)                                       #return heuristics if game is over
-            if beta <= alpha:
-                break
-        return maxEval
+            
+            
+            newState, valid,playAgain =  makeMove(stonesAmount,playerOne(moveCharP1[i]),True, True)          #3ayez makeMove teraga3 door anhy player
+         
+            if valid:
+                Eval,_ = miniMax(newState, depth - 1, alpha, beta, playAgain, playAgain)  #ab3at lel makeMove fih stealing wala la2                
+               
+                if maxEval != max(maxEval, Eval):
+                    optimalMove = moveCharP1[i]
+                    
+                maxEval = max(maxEval, Eval)                                                                         #ne7ot ehtmal en yekon el pocket fady
+                alpha = max(alpha, Eval)      
+                                                                                                                            #return heuristics if game is over
+                if beta <= alpha:
+                    break
+        return maxEval,optimalMove
+
         
         
     else:
         minEval = math.inf
         
         for i in range(6):
-            newState = makeMove(stonesAmount,moveCharP2[i],False)
-            Eval = miniMax(newState, depth - 1, alpha, beta, True)
-            minEval = min(minEval, Eval)
-            beta = min(beta, Eval)
-            if beta <= alpha:
-                break
-        return minEval
+            
+            
+            newState,valid,playAgain = makeMove(stonesAmount,playerTwo(moveCharP2[i]),False, True)
+            
+            if valid:
+                Eval,_ = miniMax(newState, depth - 1, alpha, beta, not playAgain, playAgain)      
+                minEval = min(minEval, Eval)
+                beta = min(beta, Eval)
+                
+                if beta <= alpha:
+                    break
+                
+        return minEval,-400
+
+
+
+    
