e28ce24ec18863c008d3d8eeb81a252ccb61d83f
PassantElBaroudy <76267023+PassantElBaroudy@users.noreply.github.com>
2021-06-11 23:12:32 +0200

    Update evalHeuristics.py

--- evalHeuristics.py
+++ evalHeuristics.py
@@ -1,13 +1,5 @@
-def evalHeuristics(stonesEval):
-    player1Stones = 0
-    player2Stones = 0
+def evalHeuristics(stonesEval,playAgain,isPlayerOne):
     
-    for i in range(14):
-        if i < 7:
-            player1Stones += int(stonesEval[i])
-        else:
-            player2Stones += int(stonesEval[i])
-            
-    return player1Stones - player2Stones
+    mancalaDiff = int(stonesEval[13]) - int(stonesEval[6])
 
-
+    return mancalaDiff    
